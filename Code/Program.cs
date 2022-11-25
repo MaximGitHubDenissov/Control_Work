9f5637e33f72711847d3e4c164fecf9e42675e91
Maxim Denissov <DM7894289@gmail.com>
2022-11-25 19:47:41 +0600

    Add code

---
+++ Code/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите данные (символы, буквы или числа) через запятую, без пробелов после запятой");
+string[] array = (Console.ReadLine()!.Split(','));
+string[] result = new string[array.Length];
+for (int i = 0; i < array.Length; i++){
+    if (array[i].Length <= 3)result[i] = array[i];}        
+result = result.Where(x => !string.IsNullOrEmpty(x)).ToArray(); // Убирает пустые строки из массива
+Console.WriteLine("[" + String.Join(",", array) + "]" + " ==> " + "[" + String.Join(",", result) + "]");
